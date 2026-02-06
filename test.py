df53c5face87800b63f9055236537391c0bea495
mansiKhule2331 <159242045+mansiKhule2331@users.noreply.github.com>
2026-02-06 07:13:26 +0530

    6feb

---
+++ test.py
@@ -0,0 +1,11 @@
+# print("Hello,"+input("What is your name?" )+"!")
+#how to use input function  in other function python in different way 
+# python variables to store data values in conatainers for later use.Variables is a concept in programming that allows us to give a label to a piece of data so that we can refer or referenc that data using the chosen variable name.We will see in this lesson how to create variables and how to use the variables to access the conatained value.
+# example our phone number is 1234567890 we can store this number in a variable called phone_number and then we can use the variable name to access the value of the phone number whenever we need it.
+# name=input("What is your name? ")
+# name="Mansi"
+name=input("What is your name? ")   
+print(name)
+print(len(name))
+# three line in one line 
+print(len(input("What is your name? ")))
