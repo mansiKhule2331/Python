47e87e9d276990f1bb50bc64fcac809c896dc5b2
mansi khule <mansikhule23@gmail.com>
2026-04-03 20:10:05 +0530

    Day One

---
+++ Project1/index.py
@@ -0,0 +1,11 @@
+#Python course - Day 1 - Band Name Generator
+#printing to the console
+#string manipulationand code Intelligence
+
+#Debugging and testing code
+#input function and variables
+# variable naming conventions
+print("Welcome to the Band Name Generator")
+city=input("Which city did you grow up in?\n")
+pet=input("What's the name of your first pet?\n")
+print("Your band name could be " + city + " " + pet)
